01e08640f308a66862f49ef52bced5abb62c6813
malysh1976 <semenm1305@gmail.com>
2024-01-27 16:50:45 +0300

    Ввел дополнительный параметр в боте

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import telebot 
-import requests#sgsg
+import requests
  
  
 token='6365100027:AAESUZPeAmVhDtcAPwSGJPl7IS8LINciRIs' 
@@ -21,7 +21,13 @@ def start(message):
 def getip(message): 
     ip = message.text 
     res=reqapi(ip)
-    msg=f'По вашему ip - {res["ip"]} \n Следущая информация: \n Город - {res["city"]}'
+    msg=f' По вашему ip - {res["ip"]}\
+    \n Город - {res["city"]}\
+    \n Регион - {res["region"]}\
+    \n Страна - {res["country"]}\
+    \n Координаты - {res["loc"]}\
+    \n Часовой пояс - {res["timezone"]}'
+    
     bot.send_message(message.chat.id,msg) 
 
 if __name__=='__main__': 
