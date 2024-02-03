bd5caad8a6013e773f1bedf6843ef86f90eb79c6
malysh1976 <semenm1305@gmail.com>
2024-02-03 12:10:52 +0300

    отлаживал

--- bot.py
+++ bot.py
@@ -11,25 +11,50 @@ def reqapi(ip):
     r=requests.get(url).json() 
     return(r) 
  
+def generator_keyboards(ListNameBTN, NumbersColumns=2):
+    keyboards=telebot.types.ReplyKeyboardMarkup(row_width=NumbersColumns, resize_keyboard=True)
+    btn_names=[telebot.types.KeyboardButton(text=x) for x in ListNameBTN]
+    keyboards.add(*btn_names)
+    return keyboards       
+
+
+
 @bot.message_handler(commands=['start']) 
-def start(message): 
-    msg=bot.send_message(message.chat.id,f'Привет, {message.from_user.username}\n  Введи ip:  ') 
-    bot.register_next_step_handler(msg, getip) 
- 
- 
- 
-def getip(message): 
-    ip = message.text 
-    res=reqapi(ip)
-    msg=f' По вашему ip - {res["ip"]}\
-    \n Город - {res["city"]}\
-    \n Регион - {res["region"]}\
-    \n Страна - {res["country"]}\
-    \n Координаты - {res["loc"]}\
-    \n Часовой пояс - {res["timezone"]}'
+def start(message):
+    bot.send_message(message.chat.id,f'Привет', reply_markup=generator_keyboards(['Информация об ip']))
     
-    bot.send_message(message.chat.id,msg) 
+    
+@bot.message_handler(commands=['Информация об ip']) 
+def info(message):
+    def getip(message): 
+        ip = message.text 
+        res=reqapi(ip)
+        msg=f'По вашему ip - {res["ip"]} \n Следущая информация: \n Город - {res["city"]}'
+        msg=f' По вашему ip - {res["ip"]}\
+        \n Город - {res["city"]}\
+        \n Регион - {res["region"]}\
+        \n Страна - {res["country"]}\
+        \n Координаты - {res["loc"]}\
+        \n Часовой пояс - {res["timezone"]}'
+
+
+
+
+
+
+
+
+
+#
+
+
+        
+
+
 
 if __name__=='__main__': 
     bot.infinity_polling()
+
+
+
 
